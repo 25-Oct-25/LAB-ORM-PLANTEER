32fafc7ee34d74ce66baa4691555167f45943e19
norah alminhali <norahmu515@gmail.com>
2025-11-20 06:27:20 +0300

    Done

---
+++ Planteer/plant/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from .models import Plant
+from django.db.models import Q
+
+
+
+# Create your views here.
+def add_plant_view(request: HttpRequest):
+
+   if request.method == "POST":
+      new_plant = Plant(
+         name=request.POST["name"],
+         about=request.POST["about"],
+         image=request.FILES["image"],
+         used_for=request.POST["used_for"],
+         category=request.POST["category"],
+         is_edible=request.POST["is_edible"]
+      )
+      new_plant.save()
+      return redirect('main:home_view')
+   return render(request, 'plant/create.html')
+
+
+# Detail View
+def plant_detail_view(request: HttpRequest, plant_id:int):
+   plant = Plant.objects.get(pk=plant_id)
+   related_plants = Plant.objects.filter(category=plant.category).exclude(id=plant.id)[:3]
+   return render(request, 'plant/plant_detail.html', {"plant" : plant, "related_plants": related_plants})
+
+
+# Update View
+def plant_update_view(request: HttpRequest, plant_id:int):
+
+   plant = Plant.objects.get(pk=plant_id)
+
+   if request.method == "POST":
+      plant.name = request.POST["name"]
+      plant.about = request.POST["about"]
+      if "image" in request.FILES:
+         plant.image = request.FILES["image"]
+      plant.used_for = request.POST["used_for"]
+      plant.category = request.POST["category"]
+      plant.is_edible = request.POST["is_edible"]
+      plant.save()
+
+      return redirect('plant:plant_detail_view', plant_id=plant.id)
+   
+   return render(request, "plant/plant_update.html", {"plant": plant})
+
+# Delete View
+def plant_delete_view(request: HttpRequest, plant_id:int):
+
+   plant = Plant.objects.get(pk=plant_id)
+   plant.delete()
+   return redirect('main:home_view')
+  
+
+# All Plants View
+def all_plant_view(request: HttpRequest):
+
+   plant = Plant.objects.all()
+
+   return render(request, 'plant/all_plant.html', {"plant": plant})
+
+# Search Plants View
+def search_plant_view(request:HttpRequest):
+
+   
+   query = request.GET.get("search", "")
+
+   plant = Plant.objects.filter(
+      Q(name__icontains=query) |
+      Q(category__icontains=query) |
+      Q(used_for__icontains=query) |
+      Q(about__icontains=query)
+   )
+
+   return render(request, "plant/search_plant.html", {"plant": plant,"search_query": query})
